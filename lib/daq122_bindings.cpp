7d7e5a587045d3626a2985e671ef65cb4d97980f
IPA <ya.vodzyanovskiy@lebedev.ru>
2024-09-26 14:53:54 +0300

    master - fix  bindings

--- lib/daq122_bindings.cpp
+++ lib/daq122_bindings.cpp
@@ -2,17 +2,16 @@
 #include <pybind11/stl.h>
 #include "libdaq/device/DAQ122/daq122.h"
 
-using namespace libdaq::device;
 namespace py = pybind11;
 
 PYBIND11_MODULE(daq122, m) {
-    py::class_<DAQ122>(m, "DAQ122")
+    py::class_<libdaq::device::DAQ122, std::shared_ptr<libdaq::device::DAQ122>>(m, "DAQ122")
         .def(py::init<>())
-        .def("ConnectedDevice", &DAQ122::ConnectedDevice)
-        .def("UseUSBBackend", &DAQ122::UseUSBBackend)
-        .def("InitializeDevice", &DAQ122::InitializeDevice)
-        .def("ConfigureADCParameters", &DAQ122::ConfigureADCParameters)
-        .def("ConfigADCChannel", &DAQ122::ConfigADCChannel)
-        .def("StartADCCollection", &DAQ122::StartADCCollection)
-        .def("StopADCCollection", &DAQ122::StopADCCollection);
+        .def("ConnectedDevice", &libdaq::device::DAQ122::ConnectedDevice)
+        .def("UseUSBBackend", &libdaq::device::DAQ122::UseUSBBackend)
+        .def("InitializeDevice", &libdaq::device::DAQ122::InitializeDevice)
+        .def("ConfigureADCParameters", &libdaq::device::DAQ122::ConfigureADCParameters)
+        .def("ConfigADCChannel", &libdaq::device::DAQ122::ConfigADCChannel)
+        .def("StartADCCollection", &libdaq::device::DAQ122::StartADCCollection)
+        .def("StopADCCollection", &libdaq::device::DAQ122::StopADCCollection);
 }
