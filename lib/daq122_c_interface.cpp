29f2ac54dc92697a131c4bea623132c07bad4b8a
yarvod <yar.vod.ole@gmail.com>
2024-09-26 16:13:50 +0300

    master - fixes

--- lib/daq122_c_interface.cpp
+++ lib/daq122_c_interface.cpp
@@ -7,7 +7,7 @@ DAQ122Handle DAQ122_Create() {
     return new libdaq::device::DAQ122();
 }
 
-void DAQ122_Destroy(DAQ122Handle handle) {
+void DAQ122_Delete(DAQ122Handle handle) {
     delete static_cast<libdaq::device::DAQ122*>(handle);
 }
 
@@ -23,7 +23,7 @@ bool DAQ122_InitializeDevice(DAQ122Handle handle) {
     return static_cast<libdaq::device::DAQ122*>(handle)->InitializeDevice();
 }
 
-bool DAQ122_ConfigureADCParameters(DAQ122Handle handle, int sample_rate, int voltage) {
+bool DAQ122_ConfigureSamplingParameters(DAQ122Handle handle, libdaq::device::DAQADCSampleRate sample_rate, libdaq::device::DAQVoltage voltage) {
     return static_cast<libdaq::device::DAQ122*>(handle)->ConfigureADCParameters(
         static_cast<libdaq::device::DAQADCSampleRate>(sample_rate),
         static_cast<libdaq::device::DAQVoltage>(voltage)
@@ -34,12 +34,21 @@ bool DAQ122_ConfigADCChannel(DAQ122Handle handle, uint8_t channel) {
     return static_cast<libdaq::device::DAQ122*>(handle)->ConfigADCChannel(channel);
 }
 
-bool DAQ122_StartADCCollection(DAQ122Handle handle) {
+bool DAQ122_StartCollection(DAQ122Handle handle) {
     return static_cast<libdaq::device::DAQ122*>(handle)->StartADCCollection();
 }
 
-bool DAQ122_StopADCCollection(DAQ122Handle handle) {
+bool DAQ122_StopCollection(DAQ122Handle handle) {
     return static_cast<libdaq::device::DAQ122*>(handle)->StopADCCollection();
+}
+
+bool DAQ122_TryReadData(DAQ122Handle handle, uint8_t channel, double *read_buffer, uint32_t read_size, uint32_t timeout) {
+    return static_cast<libdaq::device::DAQ122*>(handle)->TryReadData(
+        channel,
+        read_buffer,
+        read_size,
+        timeout
+    );
 }
 
 } // extern "C"
--- lib/daq122_c_interface.h
+++ lib/daq122_c_interface.h
@@ -1,5 +1,7 @@
 #ifndef DAQ122_C_INTERFACE_H
 #define DAQ122_C_INTERFACE_H
+#include <stdint.h>
+#include "libdaq/device/base_device_config.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,15 +10,16 @@ extern "C" {
 typedef void* DAQ122Handle;
 
 DAQ122Handle DAQ122_Create();
-void DAQ122_Destroy(DAQ122Handle handle);
+void DAQ122_Delete(DAQ122Handle handle);
 
 bool DAQ122_ConnectedDevice(DAQ122Handle handle);
 bool DAQ122_UseUSBBackend(DAQ122Handle handle);
 bool DAQ122_InitializeDevice(DAQ122Handle handle);
-bool DAQ122_ConfigureADCParameters(DAQ122Handle handle, int sample_rate, int voltage);
+bool DAQ122_ConfigureSamplingParameters(DAQ122Handle handle, libdaq::device::DAQADCSampleRate sample_rate, libdaq::device::DAQVoltage voltage);
 bool DAQ122_ConfigADCChannel(DAQ122Handle handle, uint8_t channel);
-bool DAQ122_StartADCCollection(DAQ122Handle handle);
-bool DAQ122_StopADCCollection(DAQ122Handle handle);
+bool DAQ122_StartCollection(DAQ122Handle handle);
+bool DAQ122_StopCollection(DAQ122Handle handle);
+bool DAQ122_TryReadData(DAQ122Handle handle, uint8_t channel, double *read_buffer, uint32_t read_size, uint32_t timeout)
 
 #ifdef __cplusplus
 }
